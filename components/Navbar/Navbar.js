7b8bd63ce0d075abbb6d7c28935fa1245f45ac45
Diego Koscky <2228939+diegokoscky@users.noreply.github.com>
2021-04-10 12:43:32 -0300

    Finalização da Navbar

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,45 +1,63 @@
+import { useState } from 'react'
 import styles from '../Navbar/Navbar.module.scss';
 import Link from 'next/link';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
 
 export default function Navbar() {
+    const [click, setClick] = useState(false);
+
+    const handleNavbar = () => {
+        setClick(!click);
+    }
+
     return (
-        <nav className={styles.navbar}>
-            <div className="container">
-                <div className={styles.navbarWrapper}>
-                    <Link href="/" passHref>
-                        <img className={styles.logoImg} 
+        <IconContext.Provider value={
+            {color: '#242424'}
+        }>
+            <nav className={styles.navbar}>
+                <div className="container">
+                    <div className={`flex-between `+styles.navbarWrapper}>
+                        <Link href="/" passHref>
+                            <img className={styles.logoImg} 
                             src="/imgs/logo.svg" 
                             alt="Logo" />
-                    </Link>
-                    <ul className="flex-center">
-                        <li>
-                            <Link href="#metodo" passHref>
-                                Método
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="#servico" passHref>
-                                Serviço
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="#portfolio" passHref>
-                                Portfólio
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="#sobre" passHref>
-                                Sobre Mim
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="#contato" passHref>
-                                Contato
-                            </Link>
-                        </li>
-                    </ul>
+                        </Link>
+                        <div className={styles.menuIcon} 
+                        onClick={handleNavbar}>
+                            { click ? <FaTimes /> : <FaBars /> }
+                        </div>
+                        <ul id={styles.navDesktop} 
+                        className={ click ? styles.navActive : 'flex-center' }>
+                            <li>
+                                <Link href="#metodo" passHref>
+                                    Método
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="#servico" passHref>
+                                    Serviço
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="#portfolio" passHref>
+                                    Portfólio
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="#sobre" passHref>
+                                    Sobre Mim
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="#contato" passHref>
+                                    Contato
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </IconContext.Provider>
     )
 }
